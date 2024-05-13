76f1a5c63645cfe984bf2540458488119a6e7d43
yunjii <162238479+itsmeyji@users.noreply.github.com>
2024-05-14 03:02:51 +0900

    Add files via upload

---
+++ 5thWeek /Queue/queueS.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "queueS.h"
+
+QueueType* createQueue() {
+	QueueType* Q;
+	Q = (QueueType*)malloc(sizeof(QueueType));
+	Q->front = -1;
+	Q->rear = -1;
+	return Q;
+}
+
+int isQueueEmpty(QueueType* Q) {
+	if (Q->front == Q->rear) {
+		printf("Queue is empty! \n\t ");
+		return 1;
+	}
+	else return 0;
+}
+
+int isQueueFull(QueueType* Q) {
+	if (Q->rear == Q_SIZE - 1) {
+		printf("Queue is full! \n\t");
+		return 1;
+	}
+	else return 0;
+}
+
+
+void enQueue(QueueType* Q, element item) {
+	if (isQueueFull(Q)) return;
+	else {
+		Q->rear++;
+		Q->queue[Q->rear] = item;
+	}
+}
+
+element peekQ(QueueType* Q) {
+	if (isQueueEmpty(Q)) return;
+	else return Q->queue[Q->front + 1];
+}
+
+void printQ(QueueType* Q) {
+	int i;
+	printf(" Queue : [");
+	for (i = Q->front + 1; i <= Q->rear; i++)
+		printf("%3c", Q->queue[i]);
+	printf(" ]");
+}[=-0];
